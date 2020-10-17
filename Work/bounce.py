a5b38dfe5dc46162dfbe2965565ff6f0e9f13e94
Spencer Duran <spencerduran@rockethomes.com>
2020-10-17 12:08:32 -0400

    bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,9 +1,9 @@
 # bounce.py
+# Exercise 1.5
 #
 height = 100
-bounces = 10
-while bounces > 0:
+bounces = 1
+while bounces <= 10:
    height = height * (3/5)
-   print(height)
-   bounces += -1
-# Exercise 1.5
+   print(bounces, round(height, 4))
+   bounces += 1
