689718979aa46c393d26cd1fad8436d87ac8762e
Spencer Duran <spencerduran@rockethomes.com>
2020-10-17 18:53:37 -0400

    sys args

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,32 @@
-# pcost.py
-#
 # Exercise 1.27
+
+import csv
+import sys
+
+def portfoliocost(filename):
+    '''
+    returns total
+    '''
+
+    total = 0
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        next(f)
+        for row in rows:
+            try:
+                totalShares = int(row[1])
+                pricePerShare = float(row[2])
+                total += (totalShares * pricePerShare)
+            except ValueError:
+                print('Bad row', row)
+
+        return total
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfoliocost(filename)
+print(cost)
